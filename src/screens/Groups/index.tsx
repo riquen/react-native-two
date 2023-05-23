96c3007462594f06063d2f1e04eada38b3375b3e
Henrique Nunes <henri.arantes1@gmail.com>
2023-05-23 20:37:46 -0300

    adding GroupCard, Highlight and ListEmpty components

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,33 @@
 import { Header } from "@components/Header";
 import { Container } from "./styles";
+import { Highlight } from "@components/Highlights";
+import { GroupCard } from "@components/GroupCard";
+import { useState } from "react";
+import { FlatList } from "react-native";
+import { ListEmpty } from "@components/ListEmpty";
 
 export function Groups() {
+  // const [groups, setGroups] = useState<string[]>(['Galera da Rocket'])
+  const [groups, setGroups] = useState<string[]>([])
+
   return (
     <Container>
       <Header />
+      <Highlight
+        title="Turmas"
+        subtitle="Jogue com a sua turma"
+      />
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard
+            title={item}
+          />
+        )}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => <ListEmpty message="Que tal cadastrar a primeira turma?" />}
+      />
     </Container>
   );
 }
